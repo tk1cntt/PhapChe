e057f7f46159afa901b99eb98020aec95cbb5107
tk1cntt <tk1cntt@gmail.com>
2026-06-18 00:52:44 +0700

    feat(85-04): export new services in barrel index
    - Export request-service, assignment-service, vault-service

--- src/lib/services/index.ts
+++ src/lib/services/index.ts
@@ -4,3 +4,6 @@
 
 export * from './request-context-builder';
 export * from './permission-service';
+export * from './request-service';
+export * from './assignment-service';
+export * from './vault-service';
